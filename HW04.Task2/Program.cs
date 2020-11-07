ee987bca10e1c8ce9d39e2a073ae357d960af33e
Oksana S <aksinja25@gmail.com>
2020-11-07 20:50:39 +0300

    added Task2

---
+++ HW04.Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW04.Task2
+{
+    class Program
+    {
+        static int Sum(int numberA, int numberB)
+        {
+            int resOfSum = numberA + numberB;
+            return resOfSum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое целое число:");
+
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе целое число:");
+
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Сумма этих чисел = "+ Sum(number1,number2));
+        }
+    }
+}
