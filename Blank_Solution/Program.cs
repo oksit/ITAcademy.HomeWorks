37b3ca4fa987a873df8b5d1e8fdffeada2b47b71
Oksana S <aksinja25@gmail.com>
2020-11-07 20:23:01 +0300

    Добавьте файлы проекта.

---
+++ Blank_Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blank_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
