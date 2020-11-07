d1023eb7849fc7f9fd1aa8c6315414fa47846d4e
Oksana S <aksinja25@gmail.com>
2020-11-07 22:46:23 +0300

    added task6

---
+++ HW04.Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HW04.Task6
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            char[] array = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            for (int i = (array.Length) - 1; i >= 0; i--)
+            { 
+                Console.WriteLine(array[i]);
+            }
+        }    
+    }
+}
