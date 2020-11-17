1cf0555e961f87641346d31c879f366c541c0a9f
Oksana S <aksinja25@gmail.com>
2020-11-17 23:50:05 +0300

    Home Task6-3 completed

---
+++ Task6-3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Task6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            var stopwatch = Stopwatch.StartNew();
+
+            Int64[] myArray = new Int64[100000000];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = rand.Next(1,100);
+                //Console.Write(myArray[i]+ " ");
+            }
+            Console.WriteLine();
+
+            MyReverse(myArray);
+
+            //Array.Reverse(myArray);
+
+            long ticks1 = stopwatch.ElapsedTicks;
+            Console.WriteLine(ticks1);
+
+            //foreach (var item in myArray)
+            //{
+            //    Console.Write(item + " ");
+            //}
+        }
+        
+        static Int64[] MyReverse(Int64[] array) 
+        {
+            Int64 temp = 0;
+            int longArr = array.Length;
+            int sub = longArr - 1;
+            for (int i = 0; i < sub; i++)
+            {
+                temp = array[i];
+                array[i] = array[sub];
+                array[sub] = temp;
+                sub--;
+            }
+
+            return array;
+        }
+    }
+}
