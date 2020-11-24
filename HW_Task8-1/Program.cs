42f8cbf895be5bb6ec0f7f2f546ed3be7bef3fe7
Oksana S <aksinja25@gmail.com>
2020-11-25 01:27:04 +0300

    home task8-1 completed

--- HW_Task8-1/Program.cs
+++ HW_Task8-1/Program.cs
@@ -8,29 +8,30 @@ namespace HW_Task8_1
         static void Main(string[] args)
         {
             string str = "1a!2.3!!.. 4.!.?6 7! ..?";
-            Console.WriteLine(str.Length);//24
-            Console.WriteLine(str.IndexOf('?'));
-            Console.WriteLine(str.LastIndexOf('?'));
-            StringBuilder sb = new StringBuilder(str);
+            Console.WriteLine("Original string: " + str);
+            Console.WriteLine();
 
+            StringBuilder sb1 = new StringBuilder(str);
+            StringBuilder sb2 = new StringBuilder(str);
 
-            int firstVoprSine = str.IndexOf('?');//первое вхождение ? = 15
-            int lastVoprSine = str.LastIndexOf('?');//второе вхождение = 23
+            int firstQuesMark = str.IndexOf('?');//первое вхождение ? = 15
+            int lastQuesMark = str.LastIndexOf('?');//второе вхождение ? = 23
 
+            sb1.Remove(firstQuesMark, 9);
+            Console.WriteLine("After remove the second part: " + sb1);
+            Console.WriteLine();
 
-            sb.Remove(0,16);
-            Console.WriteLine(sb);
-            String s2 = sb.ToString(6, 5); // s2 = "world"
+            string s1 = sb1.ToString();//output 1a!2.3!!.. 4.!.
+            sb1.Replace("!", String.Empty).Replace(".", String.Empty);
+            Console.WriteLine("After removing '!' and '.' : \n" + sb1);
+            Console.WriteLine();
 
-            sb.Remove(firstVoprSine, 9);
-            Console.WriteLine(sb);
-            sb.Replace(' ', '_');
-            Console.WriteLine(sb);
-            //str.IndexOfAny()
-            //sb.Remove
-            //sb.Replace(' ', '_');
+            sb2.Remove(0, 16);//second part
+            Console.WriteLine("After removing the first part: \n" + sb2);
+            Console.WriteLine();
+
+            sb2.Replace(' ', '_');
+            Console.WriteLine("After replace whitespace to underscore: \n" + sb2);
         }
-
-
     }
 }
