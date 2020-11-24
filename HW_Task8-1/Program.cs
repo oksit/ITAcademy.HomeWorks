04fc41eb5520e1db2f37b589973398c1ce395f2b
Oksana S <aksinja25@gmail.com>
2020-11-24 23:28:28 +0300

    Task8-2 added

---
+++ HW_Task8-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace HW_Task8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1a!2.3!!.. 4.!.?6 7! ..?";
+            Console.WriteLine(str.Length);//24
+            Console.WriteLine(str.IndexOf('?'));
+            Console.WriteLine(str.LastIndexOf('?'));
+            StringBuilder sb = new StringBuilder(str);
+
+
+            int firstVoprSine = str.IndexOf('?');//первое вхождение ? = 15
+            int lastVoprSine = str.LastIndexOf('?');//второе вхождение = 23
+
+
+            sb.Remove(0,16);
+            Console.WriteLine(sb);
+            String s2 = sb.ToString(6, 5); // s2 = "world"
+
+            sb.Remove(firstVoprSine, 9);
+            Console.WriteLine(sb);
+            sb.Replace(' ', '_');
+            Console.WriteLine(sb);
+            //str.IndexOfAny()
+            //sb.Remove
+            //sb.Replace(' ', '_');
+        }
+
+
+    }
+}
---
+++ HW_Task8-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HW_Task8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userString = "gdfgdf234dg54gf*23oP42";
+            Console.WriteLine(userString);
+            int signMult = userString.IndexOf('*');//15
+            
+            StringBuilder sb = new StringBuilder(userString);
+
+            string s1 = sb.ToString(0, 14);
+            s1 = Regex.Replace(s1, @"[^\d]", "", RegexOptions.Compiled);
+            Console.WriteLine(s1);
+            int number1 = Convert.ToInt32(s1);
+
+            string s2 = sb.ToString(16, 6);
+            s2 = Regex.Replace(s2, @"[^\d]", "", RegexOptions.Compiled);
+            Console.WriteLine(s2);
+            int number2 = Convert.ToInt32(s2);
+
+            Console.WriteLine($"{number1}*{number2} = "+ number1 * number2);
+        }
+    }
+}
